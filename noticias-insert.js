9c42ebc7991bc0b3a406f0ddb93cc7d1b97ab0b0
Roberto Herrera <betohr48@hotmail.com>
2022-11-01 15:32:47 -0600

    noticias

---
+++ noticias-insert.js
@@ -0,0 +1,51 @@
+const numXcont = 2;
+
+class noticia {
+    constructor(titulo, text, link, imagenes, alter, figcap) {
+        this.titulo = titulo;
+        this.text = text;
+        this.link = link;
+        this.images = imagenes;
+        this.alt = alter;
+        this.figcap = figcap;
+    }
+    getHTML(){
+        const articulo = document.createElement('article');
+
+        const titulo = document.createElement('h3');
+        titulo.textContent = this.titulo;
+        articulo.appendChild(titulo);
+
+        const texto = document.createElement('p');
+        texto.textContent = this.text;
+        articulo.appendChild(texto);
+
+        const figura = document.createElement('figure');
+        const link = document.createElement('a');
+        link.href = this.link;
+        const image = document.createElement('img');
+        image.alt = this.alt;
+        image.src = this.images;
+        link.appendChild(image);
+        figura.appendChild(link);
+
+        const figcap = document.createElement('figcaption')
+        figcap.textContent = this.figcap;
+        figura.appendChild(figcap);
+
+        articulo.appendChild(figura);
+        return articulo;
+    }
+}
+
+const contenedor = document.querySelector('.left')
+const noticias = []
+
+
+for(let i=0; i<numero; i++){
+    noticias.push(new noticia(titulos[i],text[i],link[i],imagenes[i],alter[i],figcap[i]))
+}
+
+for(let i=0; i<numXcont; i++){
+    contenedor.appendChild(noticias[i].getHTML());
+}
---
+++ noticias-list.js
@@ -0,0 +1,47 @@
+const numero = 3;
+
+const titulos = [
+    'En Palacio de Cultura en Tlaxcala exponen “La catrina en cuerpo y color”',
+    'Noche Mexicana y noche de bailes en Tlaxcala',
+    '#Precaución: cierres viales por desfile cívico-militar de este 16 de septiembre'
+]
+
+const text = [
+    'En el Palacio de Cultura, en la ciudad de Tlaxcala, se exhibe una serie fotográfica que muestra la belleza de las' +
+    ' catrinas mexicanas, se trata de “La catrina en cuerpo y color” que reúne más de una decena de obras que destacan' +
+    ' tanto la producción fotográfica como de recreación de este personaje de la tradición de Día de Muertos en nuestro' +
+    ' país.',
+    'Esta noche en todos los municipios de Tlaxcala, y desde luego a nivel estatal, tendrá lugar el ' +
+    ' tradicional Grito de Independencia, y tras dos años de suspensión esta vez sí habrá verbena en casi' +
+    ' todo el territorio.',
+    'A partir de las 08:00 horas de este 16 de septiembre, la Policía Estatal de Caminos y Vialidad de la' +
+    ' Secretaría de Seguridad Ciudadana realiza un cierre carretero con motivo del desfile cívico-militar.'
+]
+
+const link = [
+    'https://www.elsoldetlaxcala.com.mx/cultura/en-palacio-de-cultura-en-tlaxcala-exponen-la-catrina-en-cuerpo-y-color-8825454.html',
+    'https://www.elsoldetlaxcala.com.mx/doble-via/noche-mexicana-y-noche-de-bailes-en-tlaxcala-8894658.html',
+    'https://www.elsoldetlaxcala.com.mx/local/precaucion-cierres-viales-por-desfile-civico-militar-de-este-16-de-septiembre-8897774.html'
+]
+
+const imagenes = [
+    'images-noticias/noticia1.png',
+    'images-noticias/noticia2.png',
+    'images-noticias/noticia3.png'
+]
+
+const alter = [
+    'Imagen de noticia 1',
+    'Imagen de noticia 2',
+    'Imagen de noticia 3'
+]
+
+
+const figcap = [
+    'El palacio de Cultura presenta la exposición "La catrina en cuerpo y color", Foto: Cortesía | Erika Lemus',
+    'Hoy, en la capital del estado los tlaxcaltecas podrán disfrutar de un evento musical que será' +
+    ' encabezado por Aarón y su Grupo Ilusión. Archivo | El Sol de Tlaxcala',
+    ' De forma momentánea estará cerrada la calle Guerrero, mientras los contingentes se trasladan al' +
+    ' Centro Histórico. Diana Zempoalteca | El Sol de Tlaxcala'
+]
+

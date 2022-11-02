d75cafb045738e1c4fe877c6bdf5c3cb6bd2a77a
Roberto Herrera <betohr48@hotmail.com>
2022-11-02 00:01:17 -0600

    cambios css y lugares

---
+++ lugares-insert.js
@@ -0,0 +1,116 @@
+class lugar {
+    constructor(titulo, text, imagenes, alter, figcap, recom, punt) {
+        this.titulo = titulo;
+        this.text = text;
+        this.images = imagenes;
+        this.alt = alter;
+        this.figcap = figcap;
+        this.recom = recom;
+        this.punt = punt
+    }
+    getHTML(){
+        const articulo = document.createElement('article');
+
+        const titulo = document.createElement('h3');
+        titulo.textContent = this.titulo;
+        articulo.appendChild(titulo);
+
+        const texto = document.createElement('p');
+        texto.textContent = this.text;
+        articulo.appendChild(texto);
+
+        const figura = document.createElement('figure');
+        const image = document.createElement('img');
+        image.alt = this.alt;
+        image.src = this.images;
+        figura.appendChild(image);
+
+        const figcap = document.createElement('figcaption');
+        figcap.textContent = this.figcap;
+        figura.appendChild(figcap);
+        articulo.appendChild(figura);
+
+        const textoRecom = document.createElement('h4');
+        textoRecom.textContent = 'Nuestra recomendaciones';
+        articulo.appendChild(textoRecom);
+
+        const recomen = document.createElement('p');
+        recomen.textContent = this.recom;
+        articulo.appendChild(recomen);
+
+        const nuestraP = document.createElement('h4')
+        nuestraP.textContent = 'Nuestra puntuación';
+        articulo.appendChild(nuestraP);
+
+        for(let i=0; i<this.punt; i++){
+            const estrella = document.createElement('span')
+            estrella.classList.add('fa');
+            estrella.classList.add('fa-star');
+            articulo.appendChild(estrella);
+        }
+        return articulo;
+    }
+}
+
+function cambio(event){
+    const direccion = parseInt(event.currentTarget.dataset.index);
+    const tipo =  parseInt(event.currentTarget.parentNode.dataset.index);
+    let nextIndex = currentIndex[tipo];
+    if(direccion===0){
+        nextIndex--;
+    }else{
+        nextIndex++;
+    }
+    if (nextIndex < 0 || nextIndex === lugaresL[tipo].length) {
+        return;
+    }
+    const artitulo = lugaresL[tipo][nextIndex];
+    const caja = event.currentTarget.parentNode;
+    const seleccion = caja.querySelector('article');
+    seleccion.innerHTML = artitulo.innerHTML;
+    currentIndex[tipo] = nextIndex;
+}
+
+let currentIndex = [0, 0, 0];
+
+const arrowL = document.querySelectorAll('.lefta')
+const arrowR = document.querySelectorAll('.righta')
+
+
+const lugarRe = [];
+const lugarRE = [];
+const lugarEs = [];
+
+
+
+for(let i=0; i<numRe; i++) {
+    let lugarO = new lugar(tituloRe[i], textoRe[i], imgRe[i], altRe[i], figRe[i], recomRe[i], puntRe[i]);
+    lugarRe.push(lugarO.getHTML());
+}
+
+
+for(let i=0; i<numRE; i++){
+    let lugarO = new lugar(tituloRE[i],textoRE[i],imgRE[i],altRE[i],figRE[i],recomRE[i],puntRE[i]);
+    lugarRE.push(lugarO.getHTML());
+}
+
+for(let i=0; i<numEs; i++){
+    let lugarO = new lugar(tituloEs[i],textoEs[i],imgEs[i],altEs[i],figEs[i],recomEs[i],puntEs[i]);
+    lugarEs.push(lugarO.getHTML());
+}
+
+const lugaresL = [
+    lugarRe, lugarRE, lugarEs
+]
+
+
+for(let i=0; i<arrowR.length; i++){
+    arrowL[i].addEventListener('click',cambio)
+    arrowR[i].addEventListener('click',cambio)
+    const caja = arrowL[i].parentNode;
+    const articulo = lugaresL[i][0];
+    const seleccion = caja.querySelector('article');
+    seleccion.innerHTML = articulo.innerHTML;
+}
+
+
---
+++ lugares-list.js
@@ -0,0 +1,120 @@
+const numRe = 2;
+const numRE = 2;
+const numEs = 2;
+
+const tituloRe = [
+    'Escalinatas',
+    'Zócalo',
+
+]
+
+const textoRe = [
+    'Las escalinatas de Tlaxcala son uno de los mejores lugares para apreciar toda la ciudad ya sea' +
+    ' de mañana, día o noche. A cualquier hora es un buen momento para ir o incluso hacer ejercicio',
+    'El zócalo de la ciudad es un lugar muy bonito, donde se encuentra la catedral, el ayuntamiento y el palacio de ' +
+    'gobierno. Es un sitio muy bonito para visitar y tomar fotos.'
+]
+
+const imgRe = [
+    'images-lugares/escalinatas.png',
+    'images-lugares/zocalo.png'
+]
+
+const altRe = [
+    'Imagen de escalinatas',
+    'Imagen de zocalo'
+]
+
+const figRe = [
+    'Escalinatas de la ciudad',
+    'Zocalo de la ciudad'
+]
+
+const recomRe = [
+    'Ve de mañana o de tarde cuando sea la hora dorada, aunque de mañana encontrarás menos gente lo que es genial para' +
+    ' las fotos.',
+    'Ve de tarde o de noche, ya que es cuando la ciudad se ilumina y se ve más bonita'
+]
+
+const puntRe = [
+    5,
+    5
+]
+
+// Recorrido por el estado
+const tituloRE = [
+    'Valquirico','Malinche'
+]
+
+const textoRE = [
+    'Valquírico es un lugar muy bonito, donde se encuentra el pueblito más fotogénico de todo el pais. Es un sitio muy bonito para visitar y tomar fotos. ',
+    'Malinche es uno de los parque Nacionales mas bonitos de todo el pais. Es un sitio muy bonito para practicar senderismo y tomar fotos en especial cuando neva.'
+
+]
+
+const imgRE = [
+    'images-lugares/valquirico.png',
+    'images-lugares/malinche.png'
+
+]
+
+const altRE = [
+    'Imagen de plaza en Valquirico',
+    'Imagen de Malinche'
+]
+
+const figRE = [
+    'Plaza principal en Valquirico',
+    'Malinche, territorio compartido entre Tlaxcala y Puebla'
+
+]
+
+const recomRE = [
+    ' Ve de tarde o de noche, ya que es cuando la ciudad se ilumina. No hay transporte público así que tendrás que conducir o llegar en taxi. Es un poco costoso comprar ahí, así que ve preparado',
+    'Te recomendamos ir en fin de semana desde muy temprano, ya que si no tienes experiecia y quieres llegar a la cumbre' +
+    'te puede tomar mas de 8 horas el ascenso y descenso'
+]
+
+const puntRE = [
+    4, 5
+]
+
+
+// Escenciales
+
+const tituloEs = [
+    'Central de autobuses',
+    'Mercado de Tlaxcala'
+
+]
+
+const textoEs = [
+    'La central de autobuses es el lugar donde se encuentran todos los autobuses que salen de la ciudad. ',
+    'El lugar por excelencia para encontrar todo tipo de comida, desde tacos hasta comida china. Es el lugar perfecto para ir por lo necesario para la semana o para comer algo rico.'
+
+]
+
+const imgEs = [
+    'images-lugares/autobuses.png',
+    'images-lugares/mercado.png'
+]
+
+const altEs = [
+    'Central de autobuses de Tlaxcala',
+    'Mercado de Tlaxcala'
+]
+
+const figEs = [
+    'La recien remodelada central de autobuses',
+    'Mercado de Tlaxcala'
+]
+
+const recomEs = [
+    'Es un gran lugar para encontrar autobuses, pero si buscas más variedad o precios más económicos te recomendamos tomar un autobús hacia la CAPU ciudad de Puebla y ahí encontrarás más opciones.',
+    'Es un buen lugar, si planeas comprar la fruta mas fresca te recomendamos ir en martes. Ya que generalmente el lunes se vende productos del fin de semana anterior'
+
+]
+
+const puntEs = [
+    3, 3
+]

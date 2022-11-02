6012af4bddcd615145e667f1cc99509aa01c0e0c
Roberto Herrera <betohr48@hotmail.com>
2022-11-02 12:53:50 -0600

    expasion lugares

--- lugares-list.js
+++ lugares-list.js
@@ -1,10 +1,15 @@
-const numRe = 2;
-const numRE = 2;
-const numEs = 2;
+// Recomendados
+const numRe = 4;
+// Recorrido por el estado
+const numRE = 4;
+// Escenciales
+const numEs = 4;
 
 const tituloRe = [
     'Escalinatas',
     'Zócalo',
+    'Galerias Tlaxcala',
+    'Jardin botánico'
 
 ]
 
@@ -12,71 +17,100 @@ const textoRe = [
     'Las escalinatas de Tlaxcala son uno de los mejores lugares para apreciar toda la ciudad ya sea' +
     ' de mañana, día o noche. A cualquier hora es un buen momento para ir o incluso hacer ejercicio',
     'El zócalo de la ciudad es un lugar muy bonito, donde se encuentra la catedral, el ayuntamiento y el palacio de ' +
-    'gobierno. Es un sitio muy bonito para visitar y tomar fotos.'
+    'gobierno. Es un sitio muy bonito para visitar y tomar fotos.',
+    'Galerias Tlaxcala es un lugar muy bonito para ir a comer, ya que tiene una gran variedad de comida y es muy ' +
+    'diverso en cuanto a las tiendas para comprar o tomar un cafe',
+    'El jardin botánico es un lugar muy bonito para ir a pasear, ya que tiene una gran variedad de plantas y es muy ' +
+    'calmadado, si lo que deseas es relajarte es un buen lugar para ir con amigos o paraja.'
+
 ]
 
 const imgRe = [
     'images-lugares/escalinatas.png',
-    'images-lugares/zocalo.png'
+    'images-lugares/zocalo.png',
+    'images-lugares/galerias.png',
+    'images-lugares/jardin.png'
 ]
 
 const altRe = [
     'Imagen de escalinatas',
-    'Imagen de zocalo'
+    'Imagen de zocalo',
+    'Imagen de Galerias Tlaxcala',
+    'Imagen de jardin botánico'
 ]
 
 const figRe = [
     'Escalinatas de la ciudad',
-    'Zocalo de la ciudad'
+    'Zocalo de la ciudad',
+    'Galerias Tlaxcala',
+    'Jardin botánico'
 ]
 
 const recomRe = [
     'Ve de mañana o de tarde cuando sea la hora dorada, aunque de mañana encontrarás menos gente lo que es genial para' +
     ' las fotos.',
-    'Ve de tarde o de noche, ya que es cuando la ciudad se ilumina y se ve más bonita'
+    'Ve de tarde o de noche, ya que es cuando la ciudad se ilumina y se ve más bonita',
+    'Te recomendados usar alguna de las aplicaciones para viajes, ya que puede ser complicado usando transporte público',
+    'Te recomendamos llevar un lunch y sueter ya que puede hacer frío a lo largo del dia, pero te aseguramos te la pasarás bien'
 ]
 
 const puntRe = [
     5,
+    5,
+    4,
     5
 ]
 
 // Recorrido por el estado
 const tituloRE = [
-    'Valquirico','Malinche'
+    'Valquirico',
+    'Malinche',
+    'Huamantla',
+    'Tlaxco'
+
 ]
 
 const textoRE = [
     'Valquírico es un lugar muy bonito, donde se encuentra el pueblito más fotogénico de todo el pais. Es un sitio muy bonito para visitar y tomar fotos. ',
-    'Malinche es uno de los parque Nacionales mas bonitos de todo el pais. Es un sitio muy bonito para practicar senderismo y tomar fotos en especial cuando neva.'
+    'Malinche es uno de los parque Nacionales mas bonitos de todo el pais. Es un sitio muy bonito para practicar senderismo y tomar fotos en especial cuando neva.',
+    'Es un lugar perfecto para ir a pasar un fin de semana o para listarse previo a subir la Malinche. Es uno de los lugares con más tradición de la región.',
+    'Si eres un amante de los quesos, tortas y productos lacteos, este es lugar para ti. Tlaxco es un lugar perfecto para degustar desde quesos hasta tartas artesanales.'
 
 ]
 
 const imgRE = [
     'images-lugares/valquirico.png',
-    'images-lugares/malinche.png'
+    'images-lugares/malinche.png',
+    'images-lugares/huamantla.png',
+    'images-lugares/tlaxco.png'
 
 ]
 
 const altRE = [
     'Imagen de plaza en Valquirico',
-    'Imagen de Malinche'
+    'Imagen de Malinche',
+    'Imagen de Huamantla',
+    'Imagen de Tlaxco'
 ]
 
 const figRE = [
     'Plaza principal en Valquirico',
-    'Malinche, territorio compartido entre Tlaxcala y Puebla'
+    'Malinche, territorio compartido entre Tlaxcala y Puebla',
+    'La Huamantlada es uno de los eventos más importantes en todo el estado',
+    'Tlaxco, el lugar de los quesos'
 
 ]
 
 const recomRE = [
     ' Ve de tarde o de noche, ya que es cuando la ciudad se ilumina. No hay transporte público así que tendrás que conducir o llegar en taxi. Es un poco costoso comprar ahí, así que ve preparado',
     'Te recomendamos ir en fin de semana desde muy temprano, ya que si no tienes experiecia y quieres llegar a la cumbre' +
-    'te puede tomar mas de 8 horas el ascenso y descenso'
+    'te puede tomar mas de 8 horas el ascenso y descenso',
+    'Te recomendados usar alguna de las plataformas para busqueda de hospedaje, ya que encontrás una mejor variedad de precios y lugares',
+    'Te recomendaos ir con tiempo, ya que es un lugar muy grande y hay que recorrerlo todo. Igualmente ve preprarado para probar mucha comida'
 ]
 
 const puntRE = [
-    4, 5
+    4, 5,4,4
 ]
 
 
@@ -84,37 +118,51 @@ const puntRE = [
 
 const tituloEs = [
     'Central de autobuses',
-    'Mercado de Tlaxcala'
+    'Mercado de Tlaxcala',
+    'Plaza Vertice',
+    'Calle 20 de Noviembre'
+
+
 
 ]
 
 const textoEs = [
     'La central de autobuses es el lugar donde se encuentran todos los autobuses que salen de la ciudad. ',
-    'El lugar por excelencia para encontrar todo tipo de comida, desde tacos hasta comida china. Es el lugar perfecto para ir por lo necesario para la semana o para comer algo rico.'
+    'El lugar por excelencia para encontrar todo tipo de comida, desde tacos hasta comida china. Es el lugar perfecto para ir por lo necesario para la semana o para comer algo rico.',
+    'Plaza Vertice es el mejor lugar para cuando no sabes que hacer en un Martes en la tarde, Desde un cine hasta un centro comercial, es el lugar perfecto para ir a pasar el rato.',
+    'Si no sabes donde comprar algo, muy probablemente no has buscado en la calle 20 de Noviembre. Es el lugar perfecto para encontrar todo tipo de cosas, desde ropa hasta instrumentos musicales.'
 
 ]
 
 const imgEs = [
     'images-lugares/autobuses.png',
-    'images-lugares/mercado.png'
+    'images-lugares/mercado.png',
+    'images-lugares/vertice.png',
+    'images-lugares/20nov.png'
 ]
 
 const altEs = [
     'Central de autobuses de Tlaxcala',
-    'Mercado de Tlaxcala'
+    'Mercado de Tlaxcala',
+    'Plaza Vertice',
+    'Calle 20 de Noviembre'
 ]
 
 const figEs = [
     'La recien remodelada central de autobuses',
-    'Mercado de Tlaxcala'
+    'Mercado de Tlaxcala',
+    'Plaza Vertice',
+    'Calle 20 de Noviembre'
 ]
 
 const recomEs = [
     'Es un gran lugar para encontrar autobuses, pero si buscas más variedad o precios más económicos te recomendamos tomar un autobús hacia la CAPU ciudad de Puebla y ahí encontrarás más opciones.',
-    'Es un buen lugar, si planeas comprar la fruta mas fresca te recomendamos ir en martes. Ya que generalmente el lunes se vende productos del fin de semana anterior'
+    'Es un buen lugar, si planeas comprar la fruta mas fresca te recomendamos ir en martes. Ya que generalmente el lunes se vende productos del fin de semana anterior',
+    'Plaza vertice es un lugar muy bueno, pero tampoco es el mejor lugar para una cita o una reunion.',
+    'Es un buen lugar para comprar todo a buen precio, pero como todo centro de ciudad siempre hay que estar atento a la gente con malas intenciones.'
 
 ]
 
 const puntEs = [
-    3, 3
+    3, 3,4,4
 ]

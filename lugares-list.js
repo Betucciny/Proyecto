fb39c9d8626e52b43ab110888269baaef0bc0373
Roberto Herrera <betohr48@hotmail.com>
2022-11-08 12:06:29 -0600

    lugares-list.js actualizacion

--- lugares-list.js
+++ lugares-list.js
@@ -1,15 +1,20 @@
 // Recomendados
-const numRe = 4;
+const numRe = 7;
 // Recorrido por el estado
-const numRE = 4;
+const numRE = 8;
 // Escenciales
-const numEs = 4;
+const numEs = 10;
 
 const tituloRe = [
     'Escalinatas',
     'Zócalo',
     'Galerias Tlaxcala',
-    'Jardin botánico'
+    'Jardin botánico',
+    'Parque Temático Tizatlán',
+    'Zoológico del Altiplano',
+    'Laguna de Acuitlapilco'
+
+
 
 ]
 
@@ -21,7 +26,13 @@ const textoRe = [
     'Galerias Tlaxcala es un lugar muy bonito para ir a comer, ya que tiene una gran variedad de comida y es muy ' +
     'diverso en cuanto a las tiendas para comprar o tomar un cafe',
     'El jardin botánico es un lugar muy bonito para ir a pasear, ya que tiene una gran variedad de plantas y es muy ' +
-    'calmadado, si lo que deseas es relajarte es un buen lugar para ir con amigos o paraja.'
+    'calmadado, si lo que deseas es relajarte es un buen lugar para ir con amigos o paraja.',
+    'El lugar perfecto para subir corriendo si lo que te gusta es el ejercicio, pero si lo que deseas es tener una vista increible ' +
+    'de la ciudad, es un buen lugar para ir a pasear y tomar fotos. Te soprendera saber que es el lugar mas alto de la ciudad.',
+    'El zoológico del altiplano es un lugar muy bonito para ir a pasear, ya que tiene una gran variedad de animales y es muy ' +
+    'diverso en cuanto a las tiendas para comprar o tomar un cafe, perfecto para un domingo soleado.',
+    'La laguna de Acuitlapilco es un lugar muy bonito para ir a pasear, incluso tienes la posibilidad de rentar un Kayak y darte una vuelta  ' +
+    'por la laguna, es un lugar muy tranquilo y perfecto para ir con amigos o familia.'
 
 ]
 
@@ -29,21 +40,30 @@ const imgRe = [
     'Images/images-lugares/escalinatas.png',
     'Images/images-lugares/zocalo.png',
     'Images/images-lugares/galerias.png',
-    'Images/images-lugares/jardin.png'
+    'Images/images-lugares/jardin.png',
+    'Images/images-lugares/parqueTiza.png',
+    'Images/images-lugares/zoo.png',
+    'Images/images-lugares/laguna.png'
 ]
 
 const altRe = [
     'Imagen de escalinatas',
     'Imagen de zocalo',
     'Imagen de Galerias Tlaxcala',
-    'Imagen de jardin botánico'
+    'Imagen de jardin botánico',
+    'Imagen de parque temático',
+    'Imagen de zoológico',
+    'Imagen de laguna'
 ]
 
 const figRe = [
     'Escalinatas de la ciudad',
     'Zocalo de la ciudad',
     'Galerias Tlaxcala',
-    'Jardin botánico'
+    'Jardin botánico',
+    'Parque temático Tizatlan',
+    'Zoológico del Altiplano',
+    'Laguna de Acuitlapilco'
 ]
 
 const recomRe = [
@@ -51,14 +71,21 @@ const recomRe = [
     ' las fotos.',
     'Ve de tarde o de noche, ya que es cuando la ciudad se ilumina y se ve más bonita',
     'Te recomendados usar alguna de las aplicaciones para viajes, ya que puede ser complicado usando transporte público',
-    'Te recomendamos llevar un lunch y sueter ya que puede hacer frío a lo largo del dia, pero te aseguramos te la pasarás bien'
+    'Te recomendamos llevar un lunch y sueter ya que puede hacer frío a lo largo del dia, pero te aseguramos te la pasarás bien',
+    'Te recomendamos ir temprano para evitar el calor y la gente, pero si lo que deseas es ver la puesta de sol es un buen lugar para ir',
+    'Te recomendamos aplicar generosamente el protector solar, ya que puede hacer mucho calor y recuerda respetar a los animales',
+    'Te recomendamos llevar un balon o lunch, ya que no hay tiendas cercanas para ir a comprar'
+
 ]
 
 const puntRe = [
     5,
     5,
     4,
-    5
+    5,
+    3,
+    4,
+    3
 ]
 
 // Recorrido por el estado
@@ -66,7 +93,15 @@ const tituloRE = [
     'Valquirico',
     'Malinche',
     'Huamantla',
-    'Tlaxco'
+    'Tlaxco',
+    'Cascadas De Atlihuetzía',
+    'Cuatlapanga Jurassic',
+    'Japoneza Retreat',
+    'Centro Vacacional La Trinidad',
+
+
+
+
 
 ]
 
@@ -74,7 +109,13 @@ const textoRE = [
     'Valquírico es un lugar muy bonito, donde se encuentra el pueblito más fotogénico de todo el pais. Es un sitio muy bonito para visitar y tomar fotos. ',
     'Malinche es uno de los parque Nacionales mas bonitos de todo el pais. Es un sitio muy bonito para practicar senderismo y tomar fotos en especial cuando neva.',
     'Es un lugar perfecto para ir a pasar un fin de semana o para listarse previo a subir la Malinche. Es uno de los lugares con más tradición de la región.',
-    'Si eres un amante de los quesos, tortas y productos lacteos, este es lugar para ti. Tlaxco es un lugar perfecto para degustar desde quesos hasta tartas artesanales.'
+    'Si eres un amante de los quesos, tortas y productos lacteos, este es lugar para ti. Tlaxco es un lugar perfecto para degustar desde quesos hasta tartas artesanales.',
+    'Las cascadas de Atlihuetzía son uno de los mejores lugares para apreciar toda la naturaleza ya sea de mañana, día o noche. A cualquier hora es un buen momento para ir o incluso hacer ejercicio.',
+    'Cuatlapanga Jurassic es un lugar muy bonito para ir a pasear, ya que tiene una gran variedad de dinosaurios, te sorpendera durante todo el recorrido ' +
+    'y es muy divertido para ir con amigos o familia.',
+    'Japoneza Retreat es un lugar muy bonito para ir a pasar todo un fin de semana, ya que tiene desde un bello spa, hasta comida gourmet',
+    'Centro Vacacional La Trinidad es un lugar muy bonito para ir a pasar todo un fin de semana, desde una fabulosa alberca techada, hasta lugares para acampar y hacer fogatas',
+
 
 ]
 
@@ -82,7 +123,11 @@ const imgRE = [
     'Images/images-lugares/valquirico.png',
     'Images/images-lugares/malinche.png',
     'Images/images-lugares/huamantla.png',
-    'Images/images-lugares/tlaxco.png'
+    'Images/images-lugares/tlaxco.png',
+    'Images/images-lugares/cascadas.png',
+    'Images/images-lugares/cuatlapanga.png',
+    'Images/images-lugares/japoneza.png',
+    'Images/images-lugares/trinidad.png'
 
 ]
 
@@ -90,14 +135,22 @@ const altRE = [
     'Imagen de plaza en Valquirico',
     'Imagen de Malinche',
     'Imagen de Huamantla',
-    'Imagen de Tlaxco'
+    'Imagen de Tlaxco',
+    'Imagen de Cascadas de Atlihuetzía',
+    'Imagen de Cuatlapanga Jurassic',
+    'Imagen de Japoneza Retreat',
+    'Imagen de Centro Vacacional La Trinidad'
 ]
 
 const figRE = [
     'Plaza principal en Valquirico',
     'Malinche, territorio compartido entre Tlaxcala y Puebla',
     'La Huamantlada es uno de los eventos más importantes en todo el estado',
-    'Tlaxco, el lugar de los quesos'
+    'Tlaxco, el lugar de los quesos',
+    'Cascadas de Atlihuetzía',
+    'Cuatlapanga Jurassic',
+    'Japoneza Retreat',
+    'Centro Vacacional La Trinidad'
 
 ]
 
@@ -106,11 +159,15 @@ const recomRE = [
     'Te recomendamos ir en fin de semana desde muy temprano, ya que si no tienes experiecia y quieres llegar a la cumbre' +
     'te puede tomar mas de 8 horas el ascenso y descenso',
     'Te recomendados usar alguna de las plataformas para busqueda de hospedaje, ya que encontrás una mejor variedad de precios y lugares',
-    'Te recomendaos ir con tiempo, ya que es un lugar muy grande y hay que recorrerlo todo. Igualmente ve preprarado para probar mucha comida'
+    'Te recomendaos ir con tiempo, ya que es un lugar muy grande y hay que recorrerlo todo. Igualmente ve preprarado para probar mucha comida',
+    'te recomendamos llevar una segunda muda superior ya que puede que te mojes un poco, o si gustas puedes ir totalmente preparado para mojar tus pies',
+    'Lleva tu celular o camara con bateria completa, ya que es un lugar muy bonito para tomar fotos y no te quieres quedar sin bateria',
+    'Te recomendamos ir en un fin de semana en el que estes dispuesto a desconectarte de todo, ya que es un lugar muy bonito para relajarse',
+    'Te recomendamos ir prepararo para ir a acampar y nadar, ya que se aprovecha mejor el dia si te quedas 24 hrs en el lugar'
 ]
 
 const puntRE = [
-    4, 5,4,4
+    4, 5,4,4,3,3,4,4
 ]
 
 
@@ -120,7 +177,14 @@ const tituloEs = [
     'Central de autobuses',
     'Mercado de Tlaxcala',
     'Plaza Vertice',
-    'Calle 20 de Noviembre'
+    'Calle 20 de Noviembre',
+    'Biblioteca Central "Miguel N. Lira"',
+    'Parque Recreativo y Deportivo de La Juventud',
+    'Teatro Xicoténcatl',
+    'SAT Tlaxcala',
+    'Auditorio Emilio Sánchez Piedras',
+    'Palacio de Cultura de la Secretaria de Cultura del Gobierno Federal'
+
 
 
 
@@ -130,7 +194,13 @@ const textoEs = [
     'La central de autobuses es el lugar donde se encuentran todos los autobuses que salen de la ciudad. ',
     'El lugar por excelencia para encontrar todo tipo de comida, desde tacos hasta comida china. Es el lugar perfecto para ir por lo necesario para la semana o para comer algo rico.',
     'Plaza Vertice es el mejor lugar para cuando no sabes que hacer en un Martes en la tarde, Desde un cine hasta un centro comercial, es el lugar perfecto para ir a pasar el rato.',
-    'Si no sabes donde comprar algo, muy probablemente no has buscado en la calle 20 de Noviembre. Es el lugar perfecto para encontrar todo tipo de cosas, desde ropa hasta instrumentos musicales.'
+    'Si no sabes donde comprar algo, muy probablemente no has buscado en la calle 20 de Noviembre. Es el lugar perfecto para encontrar todo tipo de cosas, desde ropa hasta instrumentos musicales.',
+    'La biblioteca central es el lugar perfecto para ir a estudiar, ya que tiene una gran variedad de libros y es muy tranquilo.',
+    'El parque recreativo y deportivo de la juventud es el lugar perfecto para ir a hacer ejercicio, ya que tiene una gran variedad de aparatos para hacer ejercicio y lugar suficiente para un partido de futbol.',
+    'El teatro Xicoténcatl es el lugar perfecto para ir a ver una obra de teatro, ya que tiene una gran variedad de obras y es muy bonito, en donde muy constanmente tienen funciones.',
+    'Los impuestos, desde pequeños hasta grandes son necesarios, asi que esta es la oficina para ir a pagarlos.',
+    'El auditorio Emilio Sánchez Piedras es el lugar perfecto para ir a ver un concierto, ya que tiene una gran variedad de conciertos y es muy bonito, pero es tan grande que tienes muchas opciones para escoger.',
+    'El Palacio de Cultura de la Secretaria de Cultura del Gobierno Federal es el lugar perfecto para ir a ver una exposicion, ya que tiene una gran variedad de exposiciones y es muy bonito, en donde muy constanmente tienen exposiciones.'
 
 ]
 
@@ -138,31 +208,55 @@ const imgEs = [
     'Images/images-lugares/autobuses.png',
     'Images/images-lugares/mercado.png',
     'Images/images-lugares/vertice.png',
-    'Images/images-lugares/20nov.png'
+    'Images/images-lugares/20nov.png',
+    'Images/images-lugares/biblioteca.png',
+    'Images/images-lugares/parquejuventud.png',
+    'Images/images-lugares/teatro.png',
+    'Images/images-lugares/sat.png',
+    'Images/images-lugares/auditorio.png',
+    'Images/images-lugares/palacio.png'
 ]
 
 const altEs = [
     'Central de autobuses de Tlaxcala',
     'Mercado de Tlaxcala',
     'Plaza Vertice',
-    'Calle 20 de Noviembre'
+    'Calle 20 de Noviembre',
+    'Biblioteca Central "Miguel N. Lira"',
+    'Parque Recreativo y Deportivo de La Juventud',
+    'Teatro Xicoténcatl',
+    'SAT Tlaxcala',
+    'Auditorio Emilio Sánchez Piedras',
+    'Palacio de Cultura de la Secretaria de Cultura del Gobierno Federal'
 ]
 
 const figEs = [
     'La recien remodelada central de autobuses',
     'Mercado de Tlaxcala',
     'Plaza Vertice',
-    'Calle 20 de Noviembre'
+    'Calle 20 de Noviembre',
+    'Biblioteca Central "Miguel N. Lira"',
+    'Parque Recreativo y Deportivo de La Juventud',
+    'Teatro Xicoténcatl',
+    'SAT Tlaxcala',
+    'Auditorio Emilio Sánchez Piedras',
+    'Palacio de Cultura de la Secretaria de Cultura del Gobierno Federal'
 ]
 
 const recomEs = [
     'Es un gran lugar para encontrar autobuses, pero si buscas más variedad o precios más económicos te recomendamos tomar un autobús hacia la CAPU ciudad de Puebla y ahí encontrarás más opciones.',
     'Es un buen lugar, si planeas comprar la fruta mas fresca te recomendamos ir en martes. Ya que generalmente el lunes se vende productos del fin de semana anterior',
     'Plaza vertice es un lugar muy bueno, pero tampoco es el mejor lugar para una cita o una reunion.',
-    'Es un buen lugar para comprar todo a buen precio, pero como todo centro de ciudad siempre hay que estar atento a la gente con malas intenciones.'
+    'Es un buen lugar para comprar todo a buen precio, pero como todo centro de ciudad siempre hay que estar atento a la gente con malas intenciones.',
+    'Es un buen lugar, pero no buen internet así que listo para tener tu propia red de datos.',
+    'Preparate para llevar tu propio equipo, ya que no hay nada para rentar.',
+    'Es un buen lugar, pero no hay muchas opciones de comida.El complejo universitario puede tener funciones mas seguido.',
+    'No es el lugar mas divertido pero aveces es nesario estar ahí.',
+    'Gran lugar si tienes automovil propio, pero si no puede ser un poco complicado llegar.',
+    'Ahora que es parte del gobierno federal puede haber nuevas limitaciones pero aun es un buen lugar para ir.'
 
 ]
 
 const puntEs = [
-    3, 3,4,4
+    3, 3,4,4,3,4,4,3,4,4
 ]

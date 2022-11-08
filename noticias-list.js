c4a0083068bbe8dcab3331cef1773c2373c784d8
Roberto Herrera <betohr48@hotmail.com>
2022-11-07 19:39:59 -0600

    se agregan noticias

--- noticias-list.js
+++ noticias-list.js
@@ -1,12 +1,22 @@
-const numero = 3;
+const numero = 8;
 
 const titulos = [
+    'Inaugura Lorena Cuéllar Unidad Interdisciplinaria del IPN en Tlaxcala',
+    'La feria llego a Tlaxcala',
+    'Carlos Rivera cautiva a todos en sus recientes presentaciones',
+    'Héroe sin capa: Te contamos quién fue Jesús García Corona y cómo dio origen al Día del Ferrocarrilero',
+    'Será Tlaxcala sede de Olimpiada de Ciencia',
     'En Palacio de Cultura en Tlaxcala exponen “La catrina en cuerpo y color”',
     'Noche Mexicana y noche de bailes en Tlaxcala',
     '#Precaución: cierres viales por desfile cívico-militar de este 16 de septiembre'
 ]
 
 const text = [
+    'Este lunes, oficialmente fueron inauguradas las instalaciones de la Unidad Profesional Interdisciplinaria de Ingeniería (UPIIT) del Instituto Politécnico Nacional (IPN), campus Tlaxcala, en donde serán impartidos cinco programas académicos.',
+    'Tuvieron que pasar dos años de espera para que el estado nuevamente albergara a ese importante evento, de suma importancia para la cultura, preservación de tradiciones, desarrollo económico y turismo de Tlaxcala.',
+    'En el segundo concierto de Carlos Rivera en el palenque de la Feria Tlaxcala 2022, se cumplieron sueños y ellos para dos parejas que dieron el sí en la pedida de matrimonio para unir sus vidas por siempre.',
+    'El Día del Ferrocarrilero se celebra cada 7 de noviembre como un homenaje a Jesús García Corona “El Héroe de Nacozari”, un valiente maquinista de Sonora que sacrificó su vida en 1907 al sacar un tren cargado de dinamita del pueblo de Nacozari.',
+    'El Instituto Politécnico Nacional (IPN) será sede de la Olimpiada de Ciencia 2022, que se llevará a cabo del 7 al 11 de noviembre en el campus Tlaxcala, informó el director de la Unidad Profesional Interdisciplinaria de Ingeniería (UPIIT), José Luis García García.',
     'En el Palacio de Cultura, en la ciudad de Tlaxcala, se exhibe una serie fotográfica que muestra la belleza de las' +
     ' catrinas mexicanas, se trata de “La catrina en cuerpo y color” que reúne más de una decena de obras que destacan' +
     ' tanto la producción fotográfica como de recreación de este personaje de la tradición de Día de Muertos en nuestro' +
@@ -19,6 +29,11 @@ const text = [
 ]
 
 const link = [
+    'https://www.elsoldetlaxcala.com.mx/local/inaugura-lorena-cuellar-unidad-interdisciplinaria-del-ipn-en-tlaxcala-9152652.html',
+    'https://www.elsoldetlaxcala.com.mx/local/ya-inicio-la-gobernadora-lorena-cuellar-inauguro-la-gran-feria-de-tlaxcala-2022-9106207.html',
+    'https://tlaxcala.quadratin.com.mx/principal/vibra-palenque-de-la-feria-de-tlaxcala-con-carlos-rivera/',
+    'https://www.elsoldetlaxcala.com.mx/doble-via/heroe-sin-capa-te-contamos-quien-fue-jesus-garcia-corona-y-como-dio-origen-al-dia-del-ferrocarrilero-9153734.html',
+    'https://www.elsoldetlaxcala.com.mx/local/sera-tlaxcala-sede-de-olimpiada-de-ciencia-8842875.html',
     'https://www.elsoldetlaxcala.com.mx/cultura/en-palacio-de-cultura-en-tlaxcala-exponen-la-catrina-en-cuerpo-y-color-8825454.html',
     'https://www.elsoldetlaxcala.com.mx/doble-via/noche-mexicana-y-noche-de-bailes-en-tlaxcala-8894658.html',
     'https://www.elsoldetlaxcala.com.mx/local/precaucion-cierres-viales-por-desfile-civico-militar-de-este-16-de-septiembre-8897774.html'
@@ -27,17 +42,32 @@ const link = [
 const imagenes = [
     'Images/images-general/noticia1.png',
     'Images/images-general/noticia2.png',
-    'Images/images-general/noticia3.png'
+    'Images/images-general/noticia3.png',
+    'Images/images-general/noticia4.png',
+    'Images/images-general/noticia5.png',
+    'Images/images-general/noticia6.png',
+    'Images/images-general/noticia7.png',
+    'Images/images-general/noticia8.png'
 ]
 
 const alter = [
     'Imagen de noticia 1',
     'Imagen de noticia 2',
-    'Imagen de noticia 3'
+    'Imagen de noticia 3',
+    'Imagen de noticia 4',
+    'Imagen de noticia 5',
+    'Imagen de noticia 6',
+    'Imagen de noticia 7',
+    'Imagen de noticia 8'
 ]
 
 
 const figcap = [
+    'Inaugura Lorena Cuéllar Unidad Interdisciplinaria del IPN en Tlaxcala',
+    'La feria llego a Tlaxcala, La gobernadora Lorena Cuéllar inauguró la Feria de Tlaxcala 2022',
+    'Carlos Rivera cautiva a todos en sus recientes presentaciones',
+    'Héroe sin capa: Te contamos quién fue Jesús García Corona y cómo dio origen al Día del Ferrocarrilero',
+    'Será Tlaxcala sede de Olimpiada de Ciencia',
     'El palacio de Cultura presenta la exposición "La catrina en cuerpo y color", Foto: Cortesía | Erika Lemus',
     'Hoy, en la capital del estado los tlaxcaltecas podrán disfrutar de un evento musical que será' +
     ' encabezado por Aarón y su Grupo Ilusión. Archivo | El Sol de Tlaxcala',
